82012099d13526d1b6d380d2ae30357bfeffaef0
danielhe27 <jdah49d@gmail.com>
2024-03-06 19:30:10 -0500

    functions

--- Develop/server.js
+++ Develop/server.js
@@ -1,57 +1,17 @@
-const PORT = process.env.PORT || 3001;
-const path = require('path');
-const fs = require('fs');
-
 const express = require('express');
+
 const app = express();
-const filePath = path.join(__dirname, 'Develop', 'public', 'db', 'db.json');
-const allNotes = require('../Develop/public/db/db.json');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/api/notes', (req, res) => {
-  res.json(allNotes);
-});
-
-app.post('/api/notes', (req, res) => {
-  const newNote = req.body;
-  createNewNote(newNote, allNotes);
-  res.json(newNote);
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-function createNewNote(body, notesArray) {
-  if (!Array.isArray(notesArray)) {
-    notesArray = [];
-  }
-  
-  if (notesArray.length === 0) {
-    notesArray.push({ counter: 0 });
-  }
-  
-  body.id = notesArray[0].counter;
-  notesArray[0].counter++;
-  
-  notesArray.push(body);
-
-  fs.writeFileSync(filePath, JSON.stringify(notesArray, null, 2));
-  
-  return body;
-}
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('Develop/public'));
+
+app.use (express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+require('../Develop/api/notes')(app);
+require('../Develop/api/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
